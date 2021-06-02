7d48bd954bfdde190edf29dc2188f32b128069d1
Jameel Moses <jameel@interactivestrategies.com>
2021-06-02 10:17:30 -0400

    PHP 8.0: ReflectionParameter::getClass()), ::isArray(), and ::isCallable() methods deprecated

--- kint-debugger.php
+++ kint-debugger.php
@@ -3,7 +3,7 @@
  * Plugin Name: WordPress Kint Debugger
  * Plugin URI: https://github.com/jameelmoses/wordpress-kint-debugger
  * Description: Dump variables and traces in an organized and interactive display. Works with Debug Bar.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Jameel Moses
  * Author URI: https://github.com/jameelmoses
  * Requires: 2.5 or higher
